b94f15e57d283f2bc91fe602af892f9c37314620
vsheva <valera.sheva@gmail.com>
2021-12-14 02:27:58 +0200

    Функционал выбора автомобилей и показа информации,получение данных с сервера

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const selectedCar = document.getElementById('cars');
+const output = document.getElementById('output');
+const out = document.getElementById('out');
+
+const getData = (url) => {
+    return fetch(url)
+        .then((response) => response.json());
+};
+
+const cars = (data) => {
+    data.cars.forEach(element => {
+        if (element.brand === selectedCar.value) {
+            //деструктурируем объект
+            let {brand, model, price} = element;
+
+            output.textContent = `Тачка ${brand} ${model}`;
+            out.textContent = `Цена: ${price}$`;
+
+        } else if (selectedCar.value === 'none') {
+            output.textContent = 'Тачка не выбрана';
+            out.textContent = ''
+        }
+    });
+};
+
+selectedCar.addEventListener('change', () => {
+    getData('cars.json')
+        .then(cars)
+        .catch(error => output.textContent = 'Произошла ошибка: ' + error);
+});
+
+
